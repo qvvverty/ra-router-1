f3b74b2f56158480e427afe4da4e2996fb15fda1
Leonid Kostyuchenko <kstchnko@gmail.com>
2021-03-09 16:42:33 +0300

    task done

---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom";
+
+export default function Menu() {
+  return (
+    <nav className="menu">
+      <NavLink to="/" exact className="menu__item" activeClassName="menu__item-active">Главная</NavLink>
+      <NavLink to="/drift" className="menu__item" activeClassName="menu__item-active">Дрифт-такси</NavLink>
+      <NavLink to="/timeattack" className="menu__item" activeClassName="menu__item-active">Time Attack</NavLink>
+      <NavLink to="/forza" className="menu__item" activeClassName="menu__item-active">Forza Karting</NavLink>
+    </nav>
+  );
+}
